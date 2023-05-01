0df8ffe185ad7ff28c2c4bd299a51486a8569ea6
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-01 14:57:26 +0300

    feat: add routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import FavouritePage from "./pages/FavouritePage";
+import MainPage from "./pages/MainPage";
+import VacancyPage from "./pages/VacancyPage";
+import ErrorPage from "./pages/ErrorPage";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/vacancy/:id" element={<VacancyPage />}></Route>
+          <Route path="/favourites" element={<FavouritePage />}></Route>
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
