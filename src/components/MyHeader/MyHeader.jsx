32d71e5fc855c0da08e2acdf33c40108e2969b79
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-01 16:00:47 +0300

    feat: add header component

---
+++ src/components/MyHeader/MyHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Center, Container, Flex } from "@mantine/core";
+import Navigation from "../Navigation/Navigation";
+import Logo from "../UI/Logo/Logo";
+
+export default function MyHeader() {
+  return (
+    <Container size="1116px" px={0} py='24px'>
+      <Flex align='center' gap='280px'>
+        <Logo></Logo>
+        <Navigation></Navigation>
+      </Flex>
+    </Container>
+  );
+}
