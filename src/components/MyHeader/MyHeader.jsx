f44418e9339764a846c273bcfbd702105a31a52a
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-01 17:01:00 +0300

    feat: add header tag to header component

--- src/components/MyHeader/MyHeader.jsx
+++ src/components/MyHeader/MyHeader.jsx
@@ -5,11 +5,13 @@ import Logo from "../UI/Logo/Logo";
 
 export default function MyHeader() {
   return (
-    <Container size="1116px" px={0} py='24px'>
-      <Flex align='center' gap='280px'>
-        <Logo></Logo>
-        <Navigation></Navigation>
-      </Flex>
-    </Container>
+    <header>
+      <Container size="1116px" px={0} py="24px">
+        <Flex align="center" gap="280px">
+          <Logo></Logo>
+          <Navigation></Navigation>
+        </Flex>
+      </Container>
+    </header>
   );
 }
