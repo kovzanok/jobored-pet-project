dfa9415b544c6d31cbf74a58d19a97e80dd41dbb
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-01 21:30:51 +0300

    feat: add search component

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { TextInput, Button } from "@mantine/core";
+import SearchIcon from "../UI/SearchIcon";
+import classes from "./Search.module.css";
+
+export default function Search() {
+  return (
+    <TextInput
+      classNames={{
+        input: classes["search-input"],
+        icon: classes["search-icon"],
+        rightSection: classes['search-button']
+      }}
+      placeholder="Введите название вакансии"
+      rightSection={
+        <Button style={{borderRadius: '8px'}} size="lg">
+          Поиск
+        </Button>
+      }
+      rightSectionWidth={30}
+      icon={<SearchIcon />}
+    />
+  );
+}
