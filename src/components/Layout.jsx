4ea9a07cf8252433df2c098854b6a128fd70af17
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-03 23:03:22 +0300

    feat: add header component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
+import MyHeader from "./MyHeader/MyHeader.jsx";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-     <div>Header</div>
-      <Outlet />
+      <MyHeader></MyHeader>
+      <Outlet />      
     </>
   );
 }
