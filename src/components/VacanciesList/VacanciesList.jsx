82fbf059742fde8e05d9b9e80dfdfba281825811
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-03 18:55:24 +0300

    feat: vacancies are got from api

--- src/components/VacanciesList/VacanciesList.jsx
+++ src/components/VacanciesList/VacanciesList.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Vacancy from "../Vacancy";
 import { List } from "@mantine/core";
 import classes from "./VacanciesList.module.css";
+import { VacanciesContext } from "../../context/VacancyContext";
 
 export default function VacanciesList() {
+  const [vacancies] = useContext(VacanciesContext);
+  console.log(vacancies);
   return (
     <List
       classNames={{
@@ -13,12 +16,21 @@ export default function VacanciesList() {
       spacing="xl"
       listStyleType="none"
     >
-      <List.Item>
-        <Vacancy></Vacancy>
-      </List.Item>
-      <List.Item>
-        <Vacancy></Vacancy>
-      </List.Item>
+      {vacancies.map((vacancy) => {
+        const { profession, town, type_of_work,payment_from, payment_to, currency } = vacancy;
+        return (
+          <List.Item>
+            <Vacancy
+              profession={profession}
+              town={town.title}
+              typeOfWork={type_of_work.title}
+              paymentFrom={payment_from}
+              paymentTo={payment_to}
+              currency={currency}
+            ></Vacancy>
+          </List.Item>
+        );
+      })}
     </List>
   );
 }
--- src/components/Vacancy.jsx
+++ src/components/Vacancy.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { Card, Group, Text, Title } from "@mantine/core";
-import LocationIcon from './UI/LocationIcon'
-import StartIcon from './UI/StartIcon'
+import LocationIcon from "./UI/LocationIcon";
+import StartIcon from "./UI/StartIcon";
 
-export default function Vacancy() {
+export default function Vacancy({
+  profession,
+  town,
+  typeOfWork,
+  paymentFrom,
+  paymentTo,
+  currency,
+}) {
   return (
-    <Card padding='25px'>
-      <Group position="apart">
-        <Title size='2rem' weight={600} color="#5E96FC">Менеджер-дизайнер</Title>
-        <StartIcon/>
+    <Card padding="25px">
+      <Group noWrap position="apart">
+        <Title truncate='start' size="2rem" weight={600} color="#5E96FC">
+          {profession}
+        </Title>
+        <StartIcon />
       </Group>
       <Group mt={12.5} mb={12.5}>
-        <Text size='1.6rem' weight={600}>з/п от 70000 rub</Text>
+        <Text size="1.6rem" weight={600}>
+          з/п от {paymentFrom} до {paymentTo} {currency}
+        </Text>
         <Text>•</Text>
-        <Text size='1.6rem'>Полный рабочий день</Text>
+        <Text size="1.6rem">{typeOfWork}</Text>
       </Group>
       <Group>
-        <LocationIcon/>
-        <Text size='1.6rem'>Новый Уренгой</Text>
+        <LocationIcon />
+        <Text size="1.6rem">{town}</Text>
       </Group>
     </Card>
   );
