1a09510a9011eb3ba16b8e2f82d9bfdb02ed77da
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-04 13:41:39 +0300

    refactor: rename file

---
+++ src/components/Vacancies/Vacancies.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import Search from "../Search/Search";
+import VacanciesList from "../VacanciesList/VacanciesList";
+import { VacanciesContext } from "../../context/VacancyContext";
+import NotFoundMessage from "../UI/NotFoundMessage";
+
+
+export default function Vacancies() {
+  const [vacancies, , isVacanciesLoading] = useContext(VacanciesContext);
+  return (
+    <div
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Search></Search>
+      {vacancies.length === 0 && !isVacanciesLoading ? (
+        <NotFoundMessage />
+      ) : (
+        <>
+          <VacanciesList></VacanciesList>
+        </>
+      )}
+    </div>
+  );
+}
