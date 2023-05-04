48adee06678101850359bf325f9ba6a434264ff4
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-04 14:35:49 +0300

    feat: implement saved vacancies context

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,38 @@ import FavouritePage from "./pages/FavouritePage";
 import MainPage from "./pages/MainPage";
 import VacancyPage from "./pages/VacancyPage";
 import ErrorPage from "./pages/ErrorPage";
+import { ActiveVacanciesContext } from "./context/VacancyContext";
 
 import "./App.css";
 import Layout from "./components/Layout";
+import { useState } from "react";
+
+const getActiveVancancies = () => {
+  const activeVacanciesString = window.localStorage.getItem("activeVacancies");
+  if (activeVacanciesString) {
+    return JSON.parse(activeVacanciesString);
+  }
+  return [];
+};
 
 function App() {
+  const [activeVacancies, setActiveVacancies] = useState(getActiveVancancies());
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout></Layout>}>
-            <Route index element={<MainPage />}></Route>
-            <Route path="/vacancy/:id" element={<VacancyPage />}></Route>
-            <Route path="/favourites" element={<FavouritePage />}></Route>
-            <Route path="*" element={<ErrorPage />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <ActiveVacanciesContext.Provider
+        value={[activeVacancies, setActiveVacancies]}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout></Layout>}>
+              <Route index element={<MainPage />}></Route>
+              <Route path="/vacancy/:id" element={<VacancyPage />}></Route>
+              <Route path="/favourites" element={<FavouritePage />}></Route>
+              <Route path="*" element={<ErrorPage />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ActiveVacanciesContext.Provider>
     </>
   );
 }
