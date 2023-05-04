062f26907593a9a3a4be9c04779b273d5c11ca07
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-04 11:42:00 +0300

    feat: add vacancy page

--- src/pages/VacancyPage.jsx
+++ src/pages/VacancyPage.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Container, List, Card } from "@mantine/core";
+import Vacancy from "../components/Vacancy";
+import VacancyPageBody from "../components/VacancyPageBody/VacancyPageBody";
 
 export default function VacancyPage() {
   return (
-    <div>VacancyPage</div>
-  )
+    <Container size="773px" py="40px">
+      <VacancyPageBody />
+    </Container>
+  );
 }
