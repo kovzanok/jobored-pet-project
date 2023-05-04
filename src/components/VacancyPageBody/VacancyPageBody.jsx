313352774754f3aa193a6f2c752853db2760e32d
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-04 11:41:02 +0300

    feat: add vacancy page body component

---
+++ src/components/VacancyPageBody/VacancyPageBody.jsx
@@ -0,0 +1,43 @@
+import { Card, Title, List, Loader } from "@mantine/core";
+import React, { useEffect, useState } from "react";
+import classes from "./VacancyPageBody.module.css";
+import { useParams } from "react-router-dom";
+import { VacancyService } from "../../API/VacancyService";
+import Vacancy from "../Vacancy";
+
+export default function VacancyPageBody() {
+  const { id } = useParams();
+  const [vacancy, setVacancy] = useState(null);
+
+  useEffect(() => {
+    VacancyService.getVacancyById(id).then((res) => {
+      setVacancy(res);
+    });
+  }, []);
+  return (
+    <>
+      {vacancy === null ? (
+        <Loader />
+      ) : (
+        <>
+          <Vacancy
+            isVacancyPage={true}
+            id={vacancy.id}
+            profession={vacancy.profession}
+            town={vacancy.town.title}
+            typeOfWork={vacancy.type_of_work.title}
+            paymentFrom={vacancy.payment_from}
+            paymentTo={vacancy.payment_to}
+            currency={vacancy.currency}
+          />
+          <Card className={classes.card} p="24px" mt="20px">
+            <div
+              className={classes.info}
+              dangerouslySetInnerHTML={{ __html: vacancy.vacancyRichText }}
+            ></div>
+          </Card>
+        </>
+      )}
+    </>
+  );
+}
