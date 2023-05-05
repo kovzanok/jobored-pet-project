d9cfc16e5db4293222dcf1f694093d34bde1d4ae
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-05 21:34:31 +0300

    feat: create my nav link component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import classes from "./Navigation.module.css";
-import { NavLink } from "react-router-dom";
-export default function Navigation({onClose}) {
-  const setActive = ({ isActive }) => {
-    const navLinkClassName = [classes["nav-link"]];
-    if (isActive) {
-      navLinkClassName.push(classes["nav-link_active"]);
-    }
-    return navLinkClassName.join(' ');
-  };
-
+import MyNavLink from "../MyNavLink";
+export default function Navigation({ onClose }) {
   return (
     <nav className={classes["nav"]}>
-      <NavLink onClick={onClose} className={setActive} to="/">
+      <MyNavLink onClose={onClose} to="/">
         Поиск Вакансий
-      </NavLink>
-      <NavLink onClick={onClose} className={setActive} to="/favourites">
+      </MyNavLink>
+      <MyNavLink onClose={onClose} to="/favourites">
         Избранное
-      </NavLink>
+      </MyNavLink>
     </nav>
   );
 }
