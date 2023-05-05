896423d00a3407ed8ffc73666db14ffa3ec9eeec
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-05 11:45:34 +0300

    refactor: refactor search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,58 +3,57 @@ import { TextInput, Button } from "@mantine/core";
 import classes from "./Search.module.css";
 import { FiltersContext } from "../../contexts/Contexts";
 import { VacanciesContext } from "../../contexts/Contexts";
-import { VacancyService } from "../../API/VacancyService";
 import { useSearchParams } from "react-router-dom";
 import searchIcon from "../../assets/search.svg";
 
 export default function Search() {
   const [filters, setFilters] = useContext(FiltersContext);
-  const [vacancies, setVacancies, isVacanciesLoading, setIsVacanciesLoading] =
+  const [, , isVacanciesLoading, setIsVacanciesLoading] =
     useContext(VacanciesContext);
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const searchVacancies = (e) => {
+    e.preventDefault();
+    if (new URLSearchParams(filters).toString() !== searchParams.toString()) {
+      setIsVacanciesLoading(true);
+      setSearchParams(new URLSearchParams(filters));
+    }
+  };
+
+  const handleSearchInput = (e) => {
+    const newFilters = { ...filters, keyword: e.target.value };
+    if (e.target.value.length === 0) {
+      delete newFilters["keyword"];
+    }
+    setFilters({ ...newFilters });
+  };
+
   return (
-    <TextInput
-      disabled={isVacanciesLoading}
-      value={filters["keyword"] || ""}
-      onChange={(e) => {
-        const newFilters = { ...filters, keyword: e.target.value };
-        if (e.target.value.length === 0) {
-          delete newFilters["keyword"];
+    <form className={classes["form"]} onSubmit={searchVacancies}>
+      <TextInput
+        disabled={isVacanciesLoading}
+        value={filters["keyword"] || ""}
+        onChange={handleSearchInput}
+        classNames={{
+          root: classes["root"],
+          input: classes["search-input"],
+          icon: classes["search-icon"],
+          rightSection: classes["search-button"],
+        }}
+        placeholder="Введите название вакансии"
+        rightSection={
+          <Button
+            onClick={searchVacancies}
+            disabled={isVacanciesLoading}
+            style={{ borderRadius: "8px" }}
+            size="lg"
+          >
+            Поиск
+          </Button>
         }
-        setFilters({ ...newFilters });
-      }}
-      classNames={{
-        root: classes["root"],
-        input: classes["search-input"],
-        icon: classes["search-icon"],
-        rightSection: classes["search-button"],
-      }}
-      placeholder="Введите название вакансии"
-      rightSection={
-        <Button
-          disabled={isVacanciesLoading}
-          onClick={() => {
-            if (
-              new URLSearchParams(filters).toString() !==
-              searchParams.toString()
-            ) {
-              setIsVacanciesLoading(true);
-              const searchParams = new URLSearchParams(filters);
-              setSearchParams(searchParams);
-              VacancyService.getAllVacancies(searchParams).then((data) => {
-                setVacancies(data.objects);
-              });
-            }
-          }}
-          style={{ borderRadius: "8px" }}
-          size="lg"
-        >
-          Поиск
-        </Button>
-      }
-      rightSectionWidth={160}
-      icon={<img src={searchIcon} />}
-    />
+        rightSectionWidth={160}
+        icon={<img src={searchIcon} />}
+      />
+    </form>
   );
 }
