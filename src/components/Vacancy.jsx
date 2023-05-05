2b484d19bd7e26eff0721384c5c0f9a30de5d4e9
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-05 20:47:20 +0300

    feat: add adaptive styles for vacancy page

--- src/components/Vacancy.jsx
+++ src/components/Vacancy.jsx
@@ -1,19 +1,38 @@
 import React, { useContext } from "react";
-import { Card, Group, Text, Title } from "@mantine/core";
+import { Card, Group, Text, Title, createStyles } from "@mantine/core";
 import locationIcon from "../assets/location.svg";
 import StarIcon from "./UI/StarIcon";
 import { Link } from "react-router-dom";
 import { VacancyContext } from "../contexts/Contexts";
 
-
+const useStyles = createStyles(() => ({
+  title: {
+    [`@media (max-width: 600px)`]: {
+      fontSize: "2rem",
+      lineHeight: "24px",
+    },
+  },
+  salary: {
+    [`@media (max-width: 600px)`]: {
+      fontSize: "1.6rem",
+    },
+  },
+  typeOfWork: {
+    [`@media (max-width: 600px)`]: {
+      fontSize: "1.6rem",
+    },
+  },
+}));
 
 const VacancyCard = ({ isVacancyPage }) => {
   const { profession, town, type_of_work, payment_from, payment_to, currency } =
     useContext(VacancyContext);
+  const { classes } = useStyles();
   return (
     <Card padding="25px">
       <Group noWrap position="apart" align="start">
         <Title
+          className={classes.title}
           ta="start"
           size={isVacancyPage ? "2.8rem" : "2rem"}
           weight={isVacancyPage ? 700 : 600}
@@ -26,6 +45,7 @@ const VacancyCard = ({ isVacancyPage }) => {
       </Group>
       <Group mt={12.5} mb={12.5}>
         <Text
+          className={classes.salary}
           size={isVacancyPage ? "2rem" : "1.6rem"}
           weight={isVacancyPage ? 700 : 600}
           lh="2rem"
@@ -35,15 +55,16 @@ const VacancyCard = ({ isVacancyPage }) => {
           {payment_from !== 0 || payment_to !== 0 ? currency : "не указана"}
         </Text>
         <Text>•</Text>
-        <Text size={isVacancyPage ? "2rem" : "1.6rem"}>
+        <Text
+          className={classes.typeOfWork}
+          size={isVacancyPage ? "2rem" : "1.6rem"}
+        >
           {type_of_work.title}
         </Text>
       </Group>
       <Group>
         <img src={locationIcon} />
-        <Text size="1.6rem">
-          {town.title}
-        </Text>
+        <Text size="1.6rem">{town.title}</Text>
       </Group>
     </Card>
   );
