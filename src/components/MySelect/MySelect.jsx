730d09b45e5557bd21d09e0a34971b6081a22391
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-05 11:05:32 +0300

    feat: create my select component

---
+++ src/components/MySelect/MySelect.jsx
@@ -0,0 +1,51 @@
+import { Loader, Select } from "@mantine/core";
+import React, { useContext, useState, useEffect } from "react";
+import DownIcon from "../UI/DownIcon";
+import { FiltersContext, VacanciesContext } from "../../contexts/Contexts";
+import classes from "./MySelect.module.css";
+import { VacancyService } from "../../API/VacancyService";
+
+export default function MySelect() {
+  const [catalogues, setCatalogues] = useState([]);
+  const [filters, setFilters] = useContext(FiltersContext);
+  const [, , isVacanciesLoading] = useContext(VacanciesContext);
+
+  useEffect(() => {
+    VacancyService.getAllCatalogues().then((data) => {
+      setCatalogues(data);
+    });
+  }, []);
+
+  return (
+    <Select
+      value={Number(filters["catalogues"])}
+      disabled={catalogues.length === 0 || isVacanciesLoading}
+      onChange={(value) => {
+        setFilters({ ...filters, catalogues: value });
+      }}
+      maxDropdownHeight="18.8rem"
+      classNames={{
+        item: classes["item"],
+        label: classes["label"],
+        input: classes["input"],
+        rightSection: classes["rightSection"],
+        dropdown: classes["dropdown"],
+        itemsWrapper: classes["itemsWrapper"],
+      }}
+      data={catalogues.map((catalogue) => {
+        return { value: catalogue.key, label: catalogue.title };
+      })}
+      placeholder="Выберете отрасль"
+      label="Отрасль"
+      rightSection={
+        catalogues.length === 0 || isVacanciesLoading ? (
+          <Loader />
+        ) : (
+          <DownIcon />
+        )
+      }
+      rightSectionWidth={60}
+      styles={{ rightSection: { pointerEvents: "none" } }}
+    />
+  );
+}
